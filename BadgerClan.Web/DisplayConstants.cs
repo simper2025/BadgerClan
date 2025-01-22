1d3744b6ced5e5b460a47ac71793e5a33ff8f139
Alex Mickelson <alexmickelson96@gmail.com>
2025-01-22 16:31:28 -0700

    better hex layout

--- BadgerClan.Web/DisplayConstants.cs
+++ BadgerClan.Web/DisplayConstants.cs
@@ -5,7 +5,6 @@ public static class DisplayConstants
     public static readonly double Dimension = 8.0;
     public static readonly int MapWidth = 997;
     public static readonly int MapHeight = 860;
-    public static readonly double MapRatio = 997 / 860.0;
     public static readonly double Root3 = Math.Sqrt(3.0);
 
 }
