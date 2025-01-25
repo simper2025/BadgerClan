f1c595770dc6e8d67eb90ac9707d1579b1b9577b
Jonathan Allen <jallen@snow.edu>
2025-01-25 11:08:03 -0700

    Get the client ready for student use

--- BadgerClan.Client/Program.cs
+++ BadgerClan.Client/Program.cs
@@ -1,19 +1,45 @@
+using BadgerClan.Logic;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+string url = app.Configuration["ASPNETCORE_URLS"]?.Split(";")[1] ?? throw new Exception("Unable to find URL");
+int port = new Uri(url).Port;
 
-app.MapPost("/api/echo", async (HttpContext context) =>
+Console.Clear();
+Console.WriteLine("Welcome to the Sample BadgerClan Bot!");
+Console.WriteLine("Change the code in Program.cs to add custom behavior.");
+Console.WriteLine("If you're running this locally, use the following URL to join your bot:");
+Console.WriteLine();
+Console.WriteLine($"\t{url}");
+Console.WriteLine();
+Console.WriteLine("For the competition, start a DevTunnel for this port with the following command:");
+Console.WriteLine("\t winget install Microsoft.devtunnel");
+Console.WriteLine("\t [ restart your command line after installing devtunnel ]");
+Console.WriteLine("\t devtunnel user login");
+Console.WriteLine($"\t devtunnel host -p {port} --allow-anonymous");
+Console.WriteLine();
+Console.WriteLine("In the output from the 'devtunnel host' command, look for the \"Connect via browser:\" URL");
+
+
+app.MapGet("/", () => "Sample BadgerClan bot.  Modify the code in Program.cs to change how the bot performs.");
+
+app.MapPost("/", (MoveRequest request) =>
 {
-    using var reader = new StreamReader(context.Request.Body);
-    var requestBody = await reader.ReadToEndAsync();
+    app.Logger.LogInformation("Received move request for game {gameId} turn {turnNumber}", request.GameId, request.TurnNumber);
+    var myMoves = new List<Move>();
 
-    // Log or process the request data
-    Console.WriteLine($"Received: {requestBody}");
+    // ***************************************************************************
+    // ***************************************************************************
+    // **
+    // ** Your code goes right here.
+    // ** Look in the request object to see the game state.
+    // ** Then add your moves to the myMoves list.
+    // **
+    // ***************************************************************************
+    // ***************************************************************************
 
-    // Return a response
-    return Results.Json(new { message = $"You sent: {requestBody}" });
+    return new MoveResponse(myMoves);
 });
-
 
 app.Run();
--- BadgerClan.Web/Program.cs
+++ BadgerClan.Web/Program.cs
@@ -91,8 +91,3 @@ Unit FromDto(UnitDto dto)
         dto.Team
     );
 }
-
-public record JoinGameResponse(string playerName);
-public record MoveRequest(IEnumerable<UnitDto> Units, IEnumerable<int> TeamIds, int YourTeamId, int TurnNumber, string GameId, int BoardSize, int Medpacs);
-public record MoveResponse(List<Move> Moves);
-public record UnitDto(string Type, int Id, int Attack, int AttackDistance, int Health, int MaxHealth, double Moves, double MaxMoves, Coordinate Location, int Team);
