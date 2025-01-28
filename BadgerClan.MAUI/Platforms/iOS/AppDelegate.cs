ee82f4857ec1eda5f2d1827bbdf14aee2900100f
mateo <mtomaszeuski@gmail.com>
2025-01-28 15:03:28 -0700

    added API and Maui Project

---
+++ BadgerClan.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BadgerClan.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
