ee0fc07b8e284f419b46ab8f23a8addd55d93822
Dallan Simper <dallan.simper@gmail.com>
2025-02-04 20:14:10 -0700

    add some space for a dev bot

--- BadgerClan.Logic/Bot/DevBot.cs
+++ BadgerClan.Logic/Bot/DevBot.cs
@@ -5,5 +5,14 @@ public class DevBot : IBot
 {
     public int Team { get; set; }
 
-    public Task<List<Move>> PlanMovesAsync(GameState state) => Task.FromResult(new List<Move>());
+    public Task<List<Move>> PlanMovesAsync(GameState state) {
+        var moves = new List<Move>();
+        
+
+
+        
+        
+        
+        return Task.FromResult(moves);
+    }
 }
